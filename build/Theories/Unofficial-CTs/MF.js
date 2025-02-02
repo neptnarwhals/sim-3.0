a91f1f7197f724b446997bbc06230e5fd6ca6192
bigblackseal <yamiyougi@gmail.com>
2025-02-01 20:57:32 -0500

    build

--- build/Theories/Unofficial-CTs/MF.js
+++ build/Theories/Unofficial-CTs/MF.js
@@ -13,9 +13,23 @@ import Variable, { ExponentialCost } from "../../Utils/variable.js";
 import { theoryClass } from "../theory.js";
 export default function mf(data) {
     return __awaiter(this, void 0, void 0, function* () {
-        const sim = new mfSim(data);
-        const res = yield sim.simulate();
-        return res;
+        let resetMultiValues = [];
+        for (let i = 1.2; i <= 2.6; i += 0.1) {
+            resetMultiValues.push(parseFloat(i.toFixed(1)));
+        }
+        let highestRes = null;
+        let highestValue = -Infinity;
+        let bestResetMuti = 0;
+        for (const resetMulti of resetMultiValues) {
+            const sim = new mfSim(data, resetMulti);
+            const res = yield sim.simulate();
+            if (res[7] > highestValue) {
+                highestValue = res[7];
+                highestRes = res;
+                bestResetMuti = resetMulti;
+            }
+        }
+        return highestRes;
     });
 }
 const mu0 = 4e-7 * Math.PI;
@@ -31,10 +45,10 @@ class mfSim extends theoryClass {
             true,
             true,
             true,
-            ...new Array(4).fill(() => this.maxRho < this.lastPub)
+            ...new Array(4).fill(() => this.maxRho <= this.lastPub + this.vMaxBuy)
         ];
         const conditions = {
-            MF: idleStrat,
+            MF1: idleStrat,
             MF2: idleStrat,
             MF3: idleStrat,
             MF4: idleStrat
@@ -70,7 +84,7 @@ class mfSim extends theoryClass {
             [1, 1, 2, 2, 2, 1]
         ];
         const tree = {
-            MF: globalOptimalRoute,
+            MF1: globalOptimalRoute,
             MF2: globalOptimalRoute,
             MF3: globalOptimalRoute,
             MF4: globalOptimalRoute
@@ -108,6 +122,7 @@ class mfSim extends theoryClass {
         this.vz = Math.pow(10, (this.variables[7].value + this.variables[8].value - 18));
         this.vtot = Math.sqrt(this.vx * this.vx + 2 * this.vz * this.vz);
         this.resets++;
+        this.stratExtra = ": " + (this.resets) + " resets (" + parseFloat((this.t / 3600).toFixed(2)) + " hours & e" + parseFloat(this.maxRho.toFixed(2)) + " rho), " + "resetMulti= " + this.resetMulti + ", v1=" + this.variables[5].level + ", v2=" + this.variables[6].level;
     }
     updateC() {
         const xterm = l10(1e15) * this.xexp();
@@ -115,7 +130,7 @@ class mfSim extends theoryClass {
         const vterm = this.milestones[0] ? l10(3e19) * 1.3 + l10(1e6) * (this.vexp() - 1.3) : 0;
         this.c = xterm + omegaterm + vterm + l10(4.49e19);
     }
-    constructor(data) {
+    constructor(data, resetMulti) {
         super(data);
         this.pubUnlock = 8;
         this.totMult = data.rho < this.pubUnlock ? 0 : this.getTotMult(data.rho);
@@ -128,6 +143,8 @@ class mfSim extends theoryClass {
         this.vtot = 0;
         this.resets = 0;
         this.varNames = ["c1", "c2", "a1", "a2", "delta", "v1", "v2", "v3", "v4"];
+        this.stratExtra = "";
+        this.resetMulti = resetMulti;
         this.variables = [
             new Variable({ cost: new ExponentialCost(10, 2), stepwisePowerSum: { base: 2, length: 7 }, firstFreeCost: true }),
             new Variable({ cost: new ExponentialCost(1e3, 100), varBase: 2 }),
@@ -139,6 +156,24 @@ class mfSim extends theoryClass {
             new Variable({ cost: new ExponentialCost(1e50, 70), stepwisePowerSum: { default: true } }),
             new Variable({ cost: new ExponentialCost(1e55, 1e6), varBase: 1.5 }), // v4
         ];
+        // this.strat="MF"
+        switch (this.strat) {
+            case "MF1":
+                this.vMaxBuy = 1.5;
+                break;
+            case "MF2":
+                this.vMaxBuy = 3;
+                break;
+            case "MF3":
+                this.vMaxBuy = 4.5;
+                break;
+            case "MF4":
+                this.vMaxBuy = 6;
+                break;
+            default:
+                this.vMaxBuy = 0;
+                break;
+        }
         this.conditions = this.getBuyingConditions();
         this.milestoneConditions = this.getMilestoneConditions();
         this.milestoneTree = this.getMilestoneTree();
@@ -163,7 +198,7 @@ class mfSim extends theoryClass {
                 this.ticks++;
             }
             this.pubMulti = Math.pow(10, (this.getTotMult(this.pubRho) - this.totMult));
-            const result = createResult(this, "");
+            const result = createResult(this, this.stratExtra);
             while (this.boughtVars[this.boughtVars.length - 1].timeStamp > this.pubT)
                 this.boughtVars.pop();
             global.varBuy.push([result[7], this.boughtVars]);
@@ -186,23 +221,7 @@ class mfSim extends theoryClass {
         this.rho = add(this.rho, rhodot + l10(this.dt));
         const vvx = Math.pow(10, (this.variables[5].value + this.variables[6].value - 20));
         let resetcond;
-        switch (this.strat) {
-            case "MF":
-                resetcond = vvx / this.vx > 2.718 && this.maxRho < this.lastPub;
-                break;
-            case "MF2":
-                resetcond = vvx / this.vx > 3.5 && this.maxRho < this.lastPub;
-                break;
-            case "MF3":
-                resetcond = vvx / this.vx > 2.5 && this.maxRho < this.lastPub;
-                break;
-            case "MF4":
-                resetcond = vvx / this.vx > 2 && this.maxRho < this.lastPub;
-                break;
-            default:
-                resetcond = false;
-                break;
-        }
+        resetcond = vvx / this.vx > this.resetMulti;
         if (resetcond)
             this.resetParticle();
         this.t += this.dt / 1.5;
